cfa5055ddb8434cd433e0af19984fc892b3f1556
tuanAbdul <107292336+tuanAbdul@users.noreply.github.com>
2022-06-14 16:35:29 +0700

    Update mmdn.h

--- mmdn.h
+++ mmdn.h
@@ -5,18 +5,21 @@
 class mmdn{
   public:
   int x,D;
-  float y,y1,y2,y3;
+  float y,y1,y2,y3,xy;
   
   float deFuz(fuzzy MF[],int n){
     y1=0;
     y2=0;
     D=getD(MF,n);
     int i,x=0;
-    for(x=0;x<D;x++){
+    y=MF[0]._y2;
+    MF[0].calc();
+    for(x=0;x<1000;x++){
       for(i=0;i<n;i++){
         MF[i].x=x;
         MF[i].calc();
-        y=MF[0]._y2;
+      }      
+      for(i=0;i<n;i++){
         if(y<MF[i]._y2){
           y=MF[i]._y2;
         }
@@ -29,7 +32,7 @@ class mmdn{
   }
   float getD(fuzzy mf[],int n){
     int i;
-    float y;
+    float y;    
     for(i=0;i<n;i++){
       y=mf[0].d;
       if(y<mf[i].d){
@@ -39,4 +42,5 @@ class mmdn{
     return y;
   }
 };
+
 #endif
